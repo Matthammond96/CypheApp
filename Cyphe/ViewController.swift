fdcb98e3153e97484cf8a633aa456e1a9d7d1a75
Matthammond96 <ultramatt96@hotmail.co.uk>
2015-11-22 17:07:49 +0000

    Initial Commit

---
+++ Cyphe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cyphe
+//
+//  Created by Matthew Hammond on 22/11/2015.
+//  Copyright © 2015 Matthew Hammond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
